030150dbfb31c6dc6263b0252998faac7be14745
Ankush Raj <ankush2r@gmail.com>
2026-07-19 14:51:57 +0530

    Add files via upload

--- yhome.js
+++ yhome.js
@@ -1,6 +1,9 @@
 const menuBtn = document.getElementById("menuBtn");
 const mainContent = document.getElementById("content");
 const menu = document.getElementById("menu");
+const aboutMe = document.getElementById("me");
+const meContent = document.getElementById("aboutMe");
+
 let displayHome = "no";
 
 menuBtn.addEventListener("click", () => {
@@ -10,46 +13,18 @@ menuBtn.addEventListener("click", () => {
     displayHome = "yes";
     }
     else {
+        meContent.style.display = "none";
         mainContent.style.display = "block";
         menu.style.display = "none";
         displayHome = "no";
     }
 });
 
-const aboutMe = document.getElementById("me");
-const meContent = document.getElementById("aboutMe");
+const me = "no";
 
 aboutMe.addEventListener("click", () => {
-    menu.style.display = "none";
-    meContent.style.display = "block";
+        menu.style.display = "none";
+        meContent.style.display = "block";
+        mainContent.style.display = "none";
+        me = "";
 });
-
-// For Likes
-
-document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(interaction, 5000);
-});
-
-
-function interaction() {
-
-const likes = document.querySelectorAll(".like");
-const dislikes = document.querySelectorAll(".dislike");
-
-
-likes.forEach((like,index) => {
-    const dislike = dislikes[index];
-    like.addEventListener("click", () => {
-    like.innerHTML= "❤️";
-    dislike.innerHTML = "👎 0";
-});
-
-    dislike.addEventListener("click", () => {
-    like.innerHTML= "♡";
-    dislike.innerHTML = "👎 1";
-});
-
-});
-
-}
-
