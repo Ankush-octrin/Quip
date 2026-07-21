f8cd5429aa1e81a8adf360156c96dbec92ec0e6e
Ankush Raj <ankush2r@gmail.com>
2026-07-21 23:00:47 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,26 @@
+import { auth } from './jfirebase.js';
+
+import {
+    createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+
+let output = document.getElementById("output");
+
+async function createUser(auth, email, password) {
+    try{
+        console.log("waiting for the server to respond");
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+        output.textContent = "Account created successfully.";
+        window.location.replace("index.html");
+    }
+    catch(error) {
+        output.textContent = error;
+    }
+}
+
+document.getElementById("signUp").addEventListener("click", () => {
+   const pwd = document.getElementById("pwd").value.trim();
+   const email = document.getElementById("email").value.trim();
+   createUser(auth, email, pwd);
+});
